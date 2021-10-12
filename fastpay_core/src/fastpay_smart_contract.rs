e9a79d7962a9fc284c82d078fbc82367817015ae
Mathieu Baudet <mathieubaudet@fb.com>
2021-10-12 16:30:59 -0700

    fix copyright headers

--- fastpay_core/src/fastpay_smart_contract.rs
+++ fastpay_core/src/fastpay_smart_contract.rs
@@ -1,4 +1,4 @@
-// Copyright (c) Facebook Inc.
+// Copyright (c) Facebook, Inc. and its affiliates.
 // SPDX-License-Identifier: Apache-2.0
 
 //! This module is sketching a FastPay smart contract on a primary chain.
