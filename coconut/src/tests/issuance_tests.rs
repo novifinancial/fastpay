713d02515171329e21afba2ad1b037508fdbe1bf
Alberto Sonnino <asonnino@fb.com>
2022-01-12 11:50:23 +0000

    Micro bench

--- coconut/src/tests/issuance_tests.rs
+++ coconut/src/tests/issuance_tests.rs
@@ -116,3 +116,143 @@ fn aggregate_coin_fail() {
     );
     assert!(!ok);
 }
+
+// Run with:
+// cargo test --release --features micro_bench bench_ -p coconut -- --nocapture --test-threads=1
+#[cfg(feature = "micro_bench")]
+#[test]
+fn bench_issue() {
+    use statistical::{standard_deviation, mean};
+    use std::time::Instant;
+
+    const RUNS: usize = 100;
+    const PRECISION: usize = 10;
+
+    let request = request();
+    let parameters = parameters();
+    let secret_key = keypair().secret;
+
+    let mut data = Vec::new();
+    println!("benchmarking 'issue'...");
+    for _ in 0..RUNS {
+        let now = Instant::now();
+        for _ in 0..PRECISION {
+            let _result = BlindedCoins::new(
+                &parameters,
+                &secret_key,
+                &request.cms,
+                &request.cs,
+            );
+        }
+        let elapsed = now.elapsed().as_millis() as f64;
+        data.push(elapsed / PRECISION as f64); 
+    }  
+    println!("Result: {:.2} +/- {:.2} ms", mean(&data), standard_deviation(&data, None));
+}
+
+// Run with:
+// cargo test --release --features micro_bench bench_ -p coconut -- --nocapture --test-threads=1
+#[cfg(feature = "micro_bench")]
+#[test]
+fn bench_aggregate() {
+    use statistical::{standard_deviation, mean};
+    use std::time::Instant;
+
+    const RUNS: usize = 100;
+    const PRECISION: usize = 10;
+
+    let shares: Vec<_> = keypairs()
+        .iter()
+        .skip(1)
+        .map(|key| {
+            let coin = Coin::default(
+                &parameters(),
+                &key.secret,
+                /* value */ &input_attribute1().value,
+                /* seed */ &input_attribute1().seed,
+                /* key */ &input_attribute1().key,
+            );
+            (coin, key.index)
+        })
+        .collect();    
+
+    let mut data = Vec::new();
+    println!("benchmarking 'aggregate'...");
+    for _ in 0..RUNS {
+        let now = Instant::now();
+        for _ in 0..PRECISION {
+            let _result = Coin::aggregate(&shares);
+        }
+        let elapsed = now.elapsed().as_millis() as f64;
+        data.push(elapsed / PRECISION as f64); 
+    }  
+    println!("Result: {:.2} +/- {:.2} ms", mean(&data), standard_deviation(&data, None));
+}
+
+// Run with:
+// cargo test --release --features micro_bench bench_ -p coconut -- --nocapture --test-threads=1
+#[cfg(feature = "micro_bench")]
+#[test]
+fn bench_plain_verify() {
+    use statistical::{standard_deviation, mean};
+    use std::time::Instant;
+
+    const RUNS: usize = 100;
+    const PRECISION: usize = 10;
+
+    let coin = coin1(); 
+    let parameters = parameters();
+    let public_key = keypair().public; 
+    let coin_attributes = input_attribute1();
+
+    let mut data = Vec::new();
+    println!("benchmarking 'plain_verify'...");
+    for _ in 0..RUNS {
+        let now = Instant::now();
+        for _ in 0..PRECISION {
+            let _result = coin.plain_verify(
+                &parameters,
+                &public_key,
+                coin_attributes.value,
+                coin_attributes.seed,
+                coin_attributes.key,
+            );
+        }
+        let elapsed = now.elapsed().as_millis() as f64;
+        data.push(elapsed / PRECISION as f64); 
+    }  
+    println!("Result: {:.2} +/- {:.2} ms", mean(&data), standard_deviation(&data, None));
+}
+
+// Run with:
+// cargo test --release --features micro_bench bench_ -p coconut -- --nocapture --test-threads=1
+#[cfg(feature = "micro_bench")]
+#[test]
+fn bench_unblind() {
+    use statistical::{standard_deviation, mean};
+    use std::time::Instant;
+
+    const RUNS: usize = 100;
+    const PRECISION: usize = 10;
+
+    let public_key = keypair().public; 
+    let output_attributes = output_attributes();
+    let blinded_coins = BlindedCoins::new(
+        &parameters(),
+        &keypair().secret,
+        &request().cms,
+        &request().cs,
+    );
+
+    let mut data = Vec::new();
+    println!("benchmarking 'unblind'...");
+    for _ in 0..RUNS {
+        let now = Instant::now();
+        for _ in 0..PRECISION {
+            let _result = blinded_coins.unblind(&public_key, &output_attributes);
+        }
+        let elapsed = now.elapsed().as_millis() as f64;
+        data.push(elapsed / PRECISION as f64); 
+    }  
+    println!("Result: {:.2} +/- {:.2} ms", mean(&data), standard_deviation(&data, None));
+}
--- coconut/src/tests/request_tests.rs
+++ coconut/src/tests/request_tests.rs
@@ -10,3 +10,73 @@ fn verify_request() {
         .verify(&parameters(), &keypair().public, &input_keys, &offset())
         .is_ok());
 }
+
+// Run with:
+// cargo test --release --features micro_bench bench_ -p coconut -- --nocapture --test-threads=1
+#[cfg(feature = "micro_bench")]
+#[test]
+fn bench_request() {
+    use crate::fixtures::{coin1, coin2, output_attributes};
+    use rand::SeedableRng;
+    use statistical::{standard_deviation, mean};
+    use std::time::Instant;
+    use super::*;
+
+    const RUNS: usize = 100;
+    const PRECISION: usize = 10;
+
+    let parameters = parameters();
+    let input_attributes = input_attributes();
+    let output_attributes = output_attributes();
+    let public_key = keypair().public;
+    let sigmas = vec![coin1(), coin2()];
+
+    let mut data = Vec::new();
+    println!("Benchmarking 'request'...");
+    for _ in 0..RUNS {
+        let now = Instant::now();
+        for _ in 0..PRECISION {
+            let _result = CoinsRequest::new(
+                rand::rngs::StdRng::seed_from_u64(37),
+                &parameters,
+                &public_key,
+                &sigmas,
+                &input_attributes,
+                &output_attributes,
+            );
+        }
+        let elapsed = now.elapsed().as_millis() as f64;
+        data.push(elapsed / PRECISION as f64); 
+    }  
+    println!("Result: {:.2} +/- {:.2} ms", mean(&data), standard_deviation(&data, None));
+}
+
+// Run with:
+// cargo test --release --features micro_bench bench_ -p coconut -- --nocapture --test-threads=1
+#[cfg(feature = "micro_bench")]
+#[test]
+fn bench_verify_request() {
+    use statistical::{standard_deviation, mean};
+    use std::time::Instant;
+
+    const RUNS: usize = 100;
+    const PRECISION: usize = 10;
+
+    let request = request();
+    let parameters = parameters();
+    let public_key = keypair().public;
+    let input_keys = vec![input_attributes()[0].key, input_attributes()[1].key];
+    let offset = offset();
+
+    let mut data = Vec::new();
+    println!("benchmarking 'verify request'...");
+    for _ in 0..RUNS {
+        let now = Instant::now();
+        for _ in 0..PRECISION {
+            let _result = request.verify(&parameters, &public_key, &input_keys, &offset);
+        }
+        let elapsed = now.elapsed().as_millis() as f64;
+        data.push(elapsed / PRECISION as f64); 
+    }  
+    println!("Result: {:.2} +/- {:.2} ms", mean(&data), standard_deviation(&data, None));
+}
